d39cb7df0a40f85bfe22539ad6db3386d8f42ef7
AstroSW <stdsw@mail.ru>
2025-05-28 21:17:47 +0400

    ДЗ сделано

---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,38 @@
+package org.skypro.skyshop.product;
+
+import java.util.Objects;
+
+public class Product {
+    private final String productName;
+    private int price;
+
+    public Product(String productName, int price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return productName + ": " + price +" р.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(productName, product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(productName);
+    }
+}
